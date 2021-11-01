09eb79a70a5a9af5e5b0ee17139739ac8d49e764
randelsouza <randelsouza88@gmail.com>
2021-11-01 11:45:32 -0300

    formularios refeitos e modais adicionados

--- public/assets/js/cadastrar_portador.js
+++ public/assets/js/cadastrar_portador.js
@@ -1,13 +1,18 @@
 $('.select2').select2();
 
-let arrayCampos = ["name", "surname", "date-birth",
-                   "identity", "nationality", "codigopostal",
-                   "logradouro", "numerorua", "complemento",
-                   "bairro", "cidade", "estado", "telf", "email"];
+let campos = ["name", "surname", "date-birth",
+     "nacionalidade", "tipo-doc-1",
+    "doc-1", "tipo-doc-2", "doc-2",
+    "tipo-doc-3", "doc-3", "pais",
+    "codigopostal", "endereco",
+    "numero", "complemento", "bairro",
+    "municipio", "estado", "contato1", "locacao",
+    "contato2", "email"];
+
 
 
 let form = $("#form-meus-dados");
-$("#cadastro").click(function () {
+$("#cadastrar").click(function () {
 
     if (tratar_campos(arrayCampos)) {
         $.get("", form.serialize(), (data, status) => {
@@ -60,4 +65,4 @@ function limparCamposCadastroPortador() {
     $("#email").val("");
 }
 
-pegarPaises("nationality");
+pegarPaises("nacionalidade");
--- public/assets/js/meus-dados-suporte-civa.js
+++ public/assets/js/meus-dados-suporte-civa.js
@@ -34,3 +34,6 @@ $("#salvar").click(function () {
         swalAlertError(title, text, callback);
     }
 });
+
+
+pegarPaises("nacionalidade");
